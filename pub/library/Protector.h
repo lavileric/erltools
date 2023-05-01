7bcc316e85182616e11c8ef0ee4ac0b07759062d
Eric Lavillonniere <lavileric@gmail.com>
2023-05-01 23:58:47 +0200

    For Protector class, possibility to restore to a value different from entry (for iterator loops), name as class BlockEnd

--- pub/library/Protector.h
+++ pub/library/Protector.h
@@ -1,33 +1,85 @@
+/// 
+/// @file Protector.h
+/// 
+/// @brief Protector.h
+/// 
 #ifndef PROTECTOR_FILE
+    /// 
+    /// @def  PROTECTOR_FILE
+    /// 
+    /// @brief  PROTECTOR_FILE
+    /// 
 #   define PROTECTOR_FILE 
     
     template <class Protected> 
+        
+        /// 
+        /// @class  Protector
+        /// 
+        /// @brief  Protector
+        /// 
         
         class Protector {
             
             public :
             
+                /// 
+                /// @fn Protector::Protector (Protected &val) 
+                /// 
+                /// @brief Protector
+                /// 
+                /// @param [in,out] val 
+                /// 
                 Protector ( Protected &val )
                     : refVal(val),  store(val)
                 {}
                 
+                /// 
+                /// @fn Protector::Protector (Protected &val, Protected assignment) 
+                /// 
+                /// @brief Protector
+                /// 
+                /// @param [in,out] val        
+                /// @param [in,out] assignment 
+                /// 
                 Protector ( Protected &val, Protected assignment )
                     : refVal(val),  store(val)
                 {
                     val =  assignment ;
                 }
-
-                 Protector ( Protected &val, Protected assignment , Protected restore)
+                
+                /// 
+                /// @fn Protector::Protector (Protected &val, Protected assignment, Protected restore) 
+                /// 
+                /// @brief Protector
+                /// 
+                /// @param [in,out] val        
+                /// @param [in,out] assignment 
+                /// @param [in,out] restore    
+                /// 
+                Protector ( Protected &val, Protected assignment, Protected restore )
                     : refVal(val),  store(restore)
                 {
                     val =  assignment ;
                 }
                 
+                /// 
+                /// @fn Protector::~Protector () 
+                /// 
+                /// @brief Protector
+                /// 
                 ~Protector ()
                 {
                     refVal =  store ;
                 }
                 
+                /// 
+                /// @fn Protected   Protector::Stored () const 
+                /// 
+                /// @brief Stored
+                /// 
+                /// @returns  Protected
+                /// 
                 Protected Stored () const
                 {
                     return store ;
@@ -35,7 +87,32 @@
             
             private :
             
-                Protected   &refVal ;
-                Protected   store ;
+                Protected   &refVal ; ///< refVal
+                Protected   store ;   ///< store
+        };
+    
+    template <class Protected> 
+        
+        /// 
+        /// @class  BlockEnd
+        /// 
+        /// @brief  BlockEnd
+        /// 
+        
+        class BlockEnd : public Protector<Protected> {
+            
+            public :
+            
+                /// 
+                /// @fn BlockEnd::BlockEnd (Protected &val, Protected assignment) 
+                /// 
+                /// @brief BlockedEnd
+                /// 
+                /// @param [in,out] val        
+                /// @param [in,out] assignment 
+                /// 
+                BlockEnd ( Protected &val, Protected assignment )
+                    : Protector<Protected> (val, val, assignment)
+                {}
         };
 #endif
